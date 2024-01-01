00bd6ab2594a8361424e9334f317cdd411e31c49
Halcyon666 <jackchen541@sina.com>
2024-01-01 21:58:47 +0800

    fix some style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-    <div>Little Taco Shop</div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/hours">Hours</router-link>
-    <router-link to="/contact">Contact</router-link>
-    <router-link to="/about">About</router-link>
+    <div class="header__h1">Little Taco Shop</div>
+    <div class="link">
+      <router-link to="/">Home</router-link>
+      <router-link to="/hours">Hours</router-link>
+      <router-link to="/contact">Contact</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
 
     <router-view></router-view>
     <div>Copyright © 2024 Little Taco Shop</div>
@@ -14,3 +16,14 @@
 <script setup>
 // Any setup logic if needed
 </script>
+
+<style>
+@import '@/style/common.css';
+
+.link {
+  padding: var(--PADDING-TB) var(--PADDING-SIDE);
+  display: flex;
+  justify-content: space-evenly;
+  gap: 1rem;
+}
+</style>
