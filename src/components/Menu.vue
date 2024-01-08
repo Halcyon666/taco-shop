00e1cfeb584498cea9da47afae1b361ddcc5cca6
Halcyon666 <jackchen541@sina.com>
2024-01-08 22:46:07 +0800

    optimize menu element

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -7,22 +7,28 @@
         Our Tacos
       </caption>
       <thead>
+        <!-- MenuHeader -->
         <tr>
-          <th class="menu__header" scope="col">Tacos</th>
-          <th class="menu__header" scope="col">Quantity</th>
-          <th class="menu__header" scope="col">Price</th>
+          <th
+            v-for="(item, index) in menuHeaders"
+            :key="'menuheader' + index"
+            scope="col"
+            class="menu__header"
+          >
+            {{ item.content }}
+          </th>
         </tr>
       </thead>
       <tbody>
         <!-- Crunchy -->
         <tr v-for="(item, index) in crunchyItems" :key="'crunchy' + index">
-          <th class="menu__item menu__cr" scope="row" :rowspan="item.rowspan">{{ item.type }}</th>
+          <th class="menu__item menu__cr" scope="row">Crunchy</th>
           <td class="menu__item">{{ item.quantity }}</td>
           <td class="menu__item">{{ item.price }}</td>
         </tr>
         <!-- Soft -->
         <tr v-for="(item, index) in softItems" :key="'soft' + index">
-          <th class="menu__item menu__sf" scope="row" :rowspan="item.rowspan">{{ item.type }}</th>
+          <th class="menu__item menu__sf" scope="row">Soft</th>
           <td class="menu__item">{{ item.quantity }}</td>
           <td class="menu__item">{{ item.price }}</td>
         </tr>
@@ -42,22 +48,22 @@
 
 <script lang="ts" setup>
 // Define your data
+const menuHeaders = [{ content: 'Tacos' }, { content: 'Quantity' }, { content: 'Price' }]
 const crunchyItems = [
-  { type: 'Crunchy', rowspan: 3, quantity: 1, price: '$1.50' },
-  { type: 'Crunchy', rowspan: 3, quantity: 2, price: '$2.50' },
-  { type: 'Crunchy', rowspan: 3, quantity: 3, price: '$3.25' },
+  { quantity: 1, price: '$1.50' },
+  { quantity: 2, price: '$2.50' },
+  { quantity: 3, price: '$3.25' },
 ]
 
 const softItems = [
-  { type: 'Soft', rowspan: 3, quantity: 1, price: '$2.00' },
-  { type: 'Soft', rowspan: 3, quantity: 2, price: '$3.50' },
-  { type: 'Soft', rowspan: 3, quantity: 3, price: '$4.50' },
+  { quantity: 1, price: '$2.00' },
+  { quantity: 2, price: '$3.50' },
+  { quantity: 3, price: '$4.50' },
 ]
 </script>
 
 <style scoped>
 /* || MENU */
-
 thead,
 tbody,
 tfoot,
@@ -70,13 +76,13 @@ tr {
   grid-template-columns: repeat(3, 1fr);
   grid-template-areas:
     'hd1 hd2 hd3'
-    'cr cr1 cr1p'
-    'cr cr2 cr2p'
-    'cr cr3 cr3p'
-    'sf sf1 sf1p'
-    'sf sf2 sf2p'
-    'sf sf3 sf3p'
-    'cs cs cs';
+    'cr crq1 crp1'
+    'cr crq2 crp2'
+    'cr crq3 crp3'
+    'sf sfq1 sfp1'
+    'sf sfq2 sfp2'
+    'sf sfq3 sfp3'
+    'cs cs   cs';
   gap: 0.1em;
   margin-bottom: 1em;
   margin-left: 20px;
